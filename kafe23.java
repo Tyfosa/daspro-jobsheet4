718a47ad587bb893344eed13994c7d11206c713f
Tyfosa <tyfosahelga@gmail.com>
2024-09-27 14:21:25 +0700

    commit5

--- kafe23.java
+++ kafe23.java
@@ -11,7 +11,7 @@ public class kafe23 {
 
         System.out.print("Masukkan menu: ");
         menu = sc.nextLine();
-        System.out.print("Masukkan ukurancup: ");
+        System.out.print("Masukkan ukurancup (S/M/L): ");
         ukurancup = sc.next().charAt(0);
         System.out.print("Masukkan jumlah: ");
         jumlah = sc.nextInt();
@@ -21,31 +21,40 @@ public class kafe23 {
         double hargamenu = 0;
         switch (menu.toLowerCase()) {
             case "kopi":
-            hargamenu = 12000;
-            break;
+                hargamenu = 12000;
+                break;
             case "teh":
-            hargamenu= 7000;
-            break;
+                hargamenu = 7000;
+                break;
             case "coklat":
-            hargamenu= 20000;
-            break;
+                hargamenu = 20000;
+                break;
+            default:
+                System.out.println("Menu tidak tersedia.");
+                return; 
         }
+
         double totalharga = hargamenu * jumlah;
 
         switch (ukurancup) {
             case 'S':
-            break;
+                break;  
             case 'M': 
-            totalharga += 0.25 * totalharga;
-            break;
+                totalharga += 0.25 * totalharga;  
+                break;
             case 'L':
-            totalharga += 0.4 * totalharga;
-            break;
+                totalharga += 0.4 * totalharga;  
+                break;
+            default:
+               
+                System.out.println("Ukuran cup yang dipilih tidak tersedia.");
+                return; 
         }
-        double diskon = keanggotaan ? 0 : 0.1 ;
+
+        double diskon = keanggotaan ? 0 : 0.1;
         double nominalbayar = totalharga - (diskon * totalharga);
-   
+
         System.out.println("Item Pembelian: " + jumlah + " " + menu + " dengan ukuran cup " + ukurancup);
-        System.out.println("nominal bayar: " + nominalbayar );
+        System.out.println("Nominal bayar: Rp " + nominalbayar);
     }
 }
